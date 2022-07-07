73e4d8882ce178138af653104312723acd4ea07a
Sandro Jk <sandro007@gmail.com>
2022-07-07 18:45:59 -0300

    OK

--- PROMO_DO_MES.R
+++ PROMO_DO_MES.R
@@ -8,6 +8,29 @@ library(dplyr)
 con2 <- dbConnect(odbc::odbc(), "reproreplica")
 
 
+
+tab_promo_prod <-dbGetQuery(con2,"
+             WITH TB AS (SELECT TBPCODIGO FROM TABPRECO 
+             WHERE TBPDESCRICAO='PROMO DO MES JUL-AGO 22' AND TBPTABCOMB='N' AND TBPCODIGO=2085),
+             
+             PRODA AS (SELECT PROCODIGO,PRODESCRICAO PRO FROM PRODU WHERE PROSITUACAO='A'),
+            
+             
+             PRECOA AS(SELECT PROCODIGO,PREPCOVENDA2 PRA FROM PREMP WHERE EMPCODIGO=1)
+             
+             SELECT T.PROCODIGO,
+                      PRO,
+                       TBPPCDESCTO2,
+                          ROUND(PRA*(1-TBPPCDESCTO2*1.00/100)*2,2) PROMO
+                          FROM TBPPRODU T
+             INNER JOIN TB TB ON TB.TBPCODIGO=T.TBPCODIGO
+              LEFT JOIN PRODA PA ON PA.PROCODIGO=T.PROCODIGO
+                LEFT JOIN PRECOA PRA ON PRA.PROCODIGO=T.PROCODIGO  
+                             ") 
+
+View(tab_promo_prod)
+
+
 comb_promo_prod <-dbGetQuery(con2,"
              WITH TBCOMB AS (SELECT TBPCODIGO FROM TABPRECO 
              WHERE TBPDESCRICAO='PROMO DO MES JUL-AGO 22' AND TBPTABCOMB='S' AND TBPCODIGO=22),
