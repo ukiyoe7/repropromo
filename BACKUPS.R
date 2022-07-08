2a6df2a13975044b539abfd93ad95d98672d20b0
Sandro Jk <sandro007@gmail.com>
2022-07-08 10:16:04 -0300

    ok

---
+++ BACKUPS.R
@@ -0,0 +1,69 @@
+## BACK UPS
+
+
+## save files
+
+tabpromo <- dbGetQuery(con2,"SELECT * FROM TABPRECO WHERE TBPCODIGO=2085") 
+View(tabpromo)
+
+tabpromo_wd <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\tabpromo","_",format(Sys.Date(),"%d_%m_%y"),".RData")
+
+save(tabpromo,file =tabpromo_wd)  
+
+tabpromo_wd_csv <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\tabpromo","_",format(Sys.Date(),"%d_%m_%y"),".csv")
+
+write.csv2(tabpromo,file =tabpromo_wd_csv)
+
+
+## PROMO DO MES
+
+tab_promo_prod <-dbGetQuery(con2,"
+             WITH TB AS (SELECT TBPCODIGO FROM TABPRECO 
+             WHERE TBPDESCRICAO='PROMO DO MES JUL-AGO 22' AND TBPTABCOMB='N' AND TBPCODIGO=2085),
+             
+             PRODA AS (SELECT PROCODIGO,PRODESCRICAO PRO FROM PRODU WHERE PROSITUACAO='A'),
+            
+             
+             PRECOA AS(SELECT PROCODIGO,PREPCOVENDA2 PRA FROM PREMP WHERE EMPCODIGO=1)
+             
+             SELECT T.PROCODIGO,
+                      PRO,
+                       TBPPCDESCTO2,
+                          ROUND(PRA*(1-TBPPCDESCTO2*1.00/100)*2,2) PROMO
+                          FROM TBPPRODU T
+             INNER JOIN TB TB ON TB.TBPCODIGO=T.TBPCODIGO
+              LEFT JOIN PRODA PA ON PA.PROCODIGO=T.PROCODIGO
+                LEFT JOIN PRECOA PRA ON PRA.PROCODIGO=T.PROCODIGO  
+                             ") 
+
+View(tab_promo_prod)
+
+
+comb_promo_prod <-dbGetQuery(con2,"
+             WITH TBCOMB AS (SELECT TBPCODIGO FROM TABPRECO 
+             WHERE TBPDESCRICAO='PROMO DO MES JUL-AGO 22' AND TBPTABCOMB='S' AND TBPCODIGO=22),
+             
+             PRODA AS (SELECT PROCODIGO,PRODESCRICAO PROA FROM PRODU WHERE PROSITUACAO='A'),
+             
+             PRODB AS (SELECT PROCODIGO,PRODESCRICAO PROB FROM PRODU WHERE PROSITUACAO='A'),
+             
+             PRECOA AS(SELECT PROCODIGO,PREPCOVENDA2 PRA FROM PREMP WHERE EMPCODIGO=1),
+             
+             PRECOB AS(SELECT PROCODIGO,PREPCOVENDA2 PRB FROM PREMP WHERE EMPCODIGO=1)
+             
+             SELECT PROCODIGOA,
+                     PROA,
+                      CCINDICEPROA2,
+                       PROCODIGOB,
+                        PROB,
+                         CCINDICEPROB2, 
+                          ROUND(PRA*(1-CCINDICEPROA2*1.00/100)*2+PRB*(1-CCINDICEPROB2*1.00/100),2) COMBO
+                          FROM TBPCOMBPROPRO T
+             INNER JOIN TBCOMB TB ON TB.TBPCODIGO=T.TBPCODIGO
+              LEFT JOIN PRODA PA ON PA.PROCODIGO=T.PROCODIGOA
+               LEFT JOIN PRODB PB ON PB.PROCODIGO=T.PROCODIGOB
+                LEFT JOIN PRECOA PRA ON PRA.PROCODIGO=T.PROCODIGOA   
+                 LEFT JOIN PRECOB PRB ON PRB.PROCODIGO=T.PROCODIGOB 
+                             ") 
+
+View(comb_promo_prod)
--- VIEW_TABLES.R
+++ VIEW_TABLES.R
@@ -8,40 +8,22 @@ library(dplyr)
 
 con2 <- dbConnect(odbc::odbc(), "reproreplica")
 
-dbGetQuery(con2,"SELECT * FROM TABPRECO WHERE TBPCODIGO='2085'") %>% View()
+  
+  
+### TABELAS =======================================================================================================
 
-## save files
 
-tabpromo <- dbGetQuery(con2,"SELECT * FROM TABPRECO WHERE TBPDESCRICAO LIKE '%PROMO DO MES MAI-JUN 22%'") 
-  View(tabpromo)
+  
+dbGetQuery(con2,"SELECT * FROM TABPRECO WHERE TBPCODIGO=2085") %>% View()
 
-  tabpromo_wd <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\tabpromo","_",format(Sys.Date(),"%d_%m_%y"),".RData")
+
+tabpromo_prod <- dbGetQuery(con2,"SELECT * FROM TBPPRODU WHERE TBPCODIGO=2085") 
   
-  save(tabpromo,file =tabpromo_wd)  
-  
-  tabpromo_wd_csv <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\tabpromo","_",format(Sys.Date(),"%d_%m_%y"),".csv")
-  
-  
-  write.csv2(tabpromo,file =tabpromo_wd_csv )
-  
-  
-  ## TABLE PRODUCTS
-  
-  tabpromo_prod <- dbGetQuery(con2,"SELECT * FROM TBPPRODU WHERE TBPCODIGO='2085'") 
-  
-  View(tabpromo_prod)
-  
-  tabpromo_prod_wd <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\tabpromo_prod","_",format(Sys.Date(),"%d_%m_%y"),".RData")
-  
-  save(tabpromo_prod,file =tabpromo_prod_wd)  
-  
-  tabpromo_prod_wd_csv <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\tabpromo_prod","_",format(Sys.Date(),"%d_%m_%y"),".csv")
-  
-  write.csv2(tabpromo_prod,file =tabpromo_prod_wd_csv )
+View(tabpromo_prod)
   
   
   
-  tabpromo_prod <- dbGetQuery(con2,"
+tabpromo_prod <- dbGetQuery(con2,"
                              WITH PROD AS (SELECT PROCODIGO,PRODESCRICAO FROM PRODU)
                               
                               SELECT TP.*,PRODESCRICAO FROM TBPPRODU TP
@@ -52,19 +34,10 @@ tabpromo <- dbGetQuery(con2,"SELECT * FROM TABPRECO WHERE TBPDESCRICAO LIKE '%PR
   
   
   
-  # save all products
-  
   tbpprodu <- dbGetQuery(con2,"SELECT * FROM TBPPRODU") 
   
   View(tbpprodu)
-  
-  tbpprodu_wd <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\tbpprodu","_",format(Sys.Date(),"%d_%m_%y"),".RData")
-  
-  save(tbpprodu,file = tbpprodu_wd)  
-  
-  tbpprodu_wd_csv <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\tbpprodu_prod","_",format(Sys.Date(),"%d_%m_%y"),".csv")
-  
-  write.csv2(tbpprodu,file = tbpprodu_wd_csv )
+
   
   ##==================================================
 
@@ -73,66 +46,41 @@ tabpromo <- dbGetQuery(con2,"SELECT * FROM TABPRECO WHERE TBPDESCRICAO LIKE '%PR
   
   clipromo <- dbGetQuery(con2,"
                          WITH TB AS(
-                         SELECT TBPCODIGO FROM TABPRECO WHERE TBPDESCRICAO LIKE '%PROMO DO MES MAI-JUN 22%')
+                         SELECT TBPCODIGO FROM TABPRECO WHERE TBPDESCRICAO='PROMO DO MES JUL-AGO 22')
                          
                          SELECT CLICODIGO,C.TBPCODIGO FROM CLITBP C
                          INNER JOIN TB ON C.TBPCODIGO=TB.TBPCODIGO") 
    View(clipromo)
-   
-   
-   ## save clitbp
-   
-   clitbp <- dbGetQuery(con2,"SELECT * FROM CLITBP") 
-   
-   View(clitbp)
   
-   clitbp_wd <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\clitbp","_",format(Sys.Date(),"%d_%m_%y"),".RData")
-  
-  save(clitbp,file =clitbp_wd)  
-  
-  clitbp_wd_csv <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\clitbp","_",format(Sys.Date(),"%d_%m_%y"),".csv")
+
   
   
-  write.csv2(clitbp,file =clitbp_wd_csv )
-  
-  
-  # clitbp comb
+### COMBINADOS =======================================================================================================
   
   
   ## save clitbp
   
-  clitbpcomb <- dbGetQuery(con2,"SELECT * FROM CLITBPCOMB") 
+  clitbpcomb <- dbGetQuery(con2,"
+                           WITH TBCOMB AS (SELECT TBPCODIGO FROM TABPRECO 
+                            WHERE TBPDESCRICAO='PROMO DO MES JUL-AGO 22' AND TBPTABCOMB='S' AND TBPCODIGO<>22)
+                           
+                           
+                           SELECT * FROM CLITBPCOMB T
+                           INNER JOIN TBCOMB TB ON TB.TBPCODIGO=T.TBPCODIGO
+                           ") 
   
   View(clitbpcomb)
   
-  clitbpcomb_wd <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\clitbpcomb","_",format(Sys.Date(),"%d_%m_%y"),".RData")
   
-  save(clitbpcomb,file =clitbpcomb_wd)  
-  
-  clitbpcomb_wd_csv <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\clitbpcomb","_",format(Sys.Date(),"%d_%m_%y"),".csv")
-  
-  
-  write.csv2(clitbpcomb,file =clitbpcomb_wd_csv )
-  
-  
-  dbGetQuery(con2,"SELECT * FROM TABPRECO WHERE TBPDESCRICAO LIKE '%PROMO DO MES MAI-JUN 22%' AND TBPTABCOMB='S'") %>% View()
+  dbGetQuery(con2,"SELECT * FROM TABPRECO WHERE TBPDESCRICAO='PROMO DO MES JUL-AGO 22' AND TBPTABCOMB='S'") %>% 
+    View()
   
 
   ## save tbpcombpropro
   
-  tbpcombpropro <- dbGetQuery(con2,"SELECT * FROM TBPCOMBPROPRO") 
-  
-  View(tbpcombpropro)
-  
-  tbpcombpropro_wd <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\tbpcombpropro","_",format(Sys.Date(),"%d_%m_%y"),".RData")
-  
-  save(tbpcombpropro,file =tbpcombpropro_wd)  
-  
-  tbpcombpropro_wd_csv <-  paste0("C:\\Users\\Repro\\Documents\\R\\ADM\\PROMODOMES\\BASES\\tbpcombpropro","_",format(Sys.Date(),"%d_%m_%y"),".csv")
-  
-  
-  write.csv2(tbpcombpropro,file =tbpcombpropro_wd_csv )
-  
+  tbpcombpropro <- dbGetQuery(con2,"SELECT * FROM TBPCOMBPROPRO WHERE TBPCODIGO IN (22)")
+
+
   
   dbGetQuery(con2,"SELECT * FROM TABPRECO 
              WHERE TBPDESCRICAO LIKE '%PROMO DO MES MAI-JUN 22%' AND TBPTABCOMB='S'") %>% View()
@@ -142,11 +90,12 @@ tabpromo <- dbGetQuery(con2,"SELECT * FROM TABPRECO WHERE TBPDESCRICAO LIKE '%PR
   
   comb_promo_prod <-dbGetQuery(con2,"
              WITH TBCOMB AS (SELECT TBPCODIGO FROM TABPRECO 
-             WHERE TBPDESCRICAO LIKE '%PROMO DO MES MAI-JUN 22%' AND TBPTABCOMB='S')
+             WHERE TBPDESCRICAO='PROMO DO MES JUL-AGO 22' AND TBPTABCOMB='S')
+          
              
-             SELECT T.TBPCODIGO FROM TBPCOMBPROPRO T
+             SELECT * FROM TBPCOMBPROPRO T
              INNER JOIN TBCOMB TB ON TB.TBPCODIGO=T.TBPCODIGO
-             WHERE PROCODIGOA='LD0471'   
+             
                         ") 
   
   View(comb_promo_prod)
